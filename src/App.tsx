b0bf14cb0046b45464067ea2d39b511ce6520640
listopadgeorgii <listopadiogl@gmail.com>
2022-12-24 18:53:50 +0300

    rendering in log

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App rendering')
+    return (
+        <div>
+            <AppTitile />
+            <Rating />
+            <Accordion/>
+            <Rating />
+        </div>
+    );
+}
+function AppTitile () {
+    console.log('AppTitle rendering')
+    return <>This is App component!</>
+}
+function Rating() {
+    console.log('Rating rendering')
+    return (
+        <>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </>
+    );
+
 }
 
+function Star() {
+    console.log('star rendering')
+    return <div>star</div>
+}
+
+function Accordion() {
+    console.log('Accordion rendering')
+        return (
+            <>
+    <AccordionTitle />
+    <AccordionBody />
+            </>
+        )
+}
+
+function AccordionTitle () {
+    console.log('AccordionTitle rendering')
+    return <h3>Меню</h3>
+}
+
+function AccordionBody () {
+    console.log('AccordionBody rendering')
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,58 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+// let a = {
+//     name: 'Georgii',
+//     surname: 'Listopad',
+//         adress: {
+//             city: 'Zelenograd',
+//             country: 'Russia '
+//         }
+// };
+//
+// let users = [
+//     {
+//     name: 'Georgii',
+//     surname: 'Listopad',
+//     adress: {
+//         city: 'Zelenograd',
+//         country: 'Russia '
+//     }
+// },
+//
+//     {
+//         name: 'George',
+//         surname: 'Listopades',
+//         adress: {
+//             city: 'Los-Angeles',
+//             country: 'USA'
+//         }
+//
+//     }
+//
+// ]
+//
+// let b : Array<boolean | number | string>;
+//
+// b = [1,2,3,4,5];
+// b = ['any', 'str']
+// b = [8,16,32,1675]
+// b = [true, false]
+//
+//
+//
+//
+// console.log(users[0].adress.country)
