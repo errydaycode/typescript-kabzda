2348bca6e5e3f09fa35b86b3e14d1e30fc1e79b1
listopadgeorgii <listopadiogl@gmail.com>
2023-04-02 23:53:26 +0300

    StoryBookEbany!

---
+++ src/components/AppTitle/AppTitle.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {AppTitle} from "./AppTitle";
+import exp from "constants";
+
+export default {
+    title: 'Title',
+    component: AppTitle
+}
+
+export const Default =()=> <AppTitle title={'xD'}/>
---
+++ src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Rating} from "./Rating";
+
+
+
+export default {
+    title: 'Rating stories',
+    component: Rating,
+}
+
+
+
+export const Rating1 = () => <Rating value={1} onClick={x=> x}/>
+
+
+
+
+// const Template = (args: RatingPropsType) => <Rating {...args} />;
+
+// export const Primary = Template.bind({});
+// Primary.args = {
+//     primary: true,
+//     label: 'Button',
+// };
+//
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//     label: 'Button',
+// };
+//
+// export const Large = Template.bind({});
+// Large.args = {
+//     size: 'large',
+//     label: 'Button',
+// };
+//
+// export const Small = Template.bind({});
+// Small.args = {
+//     size: 'small',
+//     label: 'Button',
+// };
