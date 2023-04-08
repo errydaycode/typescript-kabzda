1f7ffbccd3a63fea4672bb976c7d608f2368527a
listopadgeorgii <listopadiogl@gmail.com>
2023-04-08 13:48:23 +0300

    13 - controlled input, checkbox, select

--- src/stories/assets/Input.stories.tsx
+++ src/stories/assets/Input.stories.tsx
@@ -40,6 +40,44 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     <input ref={inputRef} />
     <button onClick={onSaveHandler}>save</button>- actual value: {value} </>
 }
+
+
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('')
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>)=> { (setParentValue(e.currentTarget.value) ) }
+
+  return <input value={parentValue} onChange={onChange }/>
+
+
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState<boolean>(true)
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>)=> { (setParentValue(e.currentTarget.checked) ) }
+
+  return <input type={'checkbox'} checked={parentValue} onChange={onChange }/>
+
+
+}
+
+export const ControlledSelect = () => {
+
+  const [parentValue, setParentValue] = useState<string | undefined>('2')
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>)=> { (setParentValue(e.currentTarget.value) ) }
+
+  return <select value={parentValue} onChange={onChange}>
+    <option>none</option>
+    <option value="1">Minsk</option>
+    <option value="2">Moscow</option>
+    <option value="3">Yerevan</option>
+  </select>
+
+
+}
+
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
 
 
