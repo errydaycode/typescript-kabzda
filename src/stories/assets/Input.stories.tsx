3431142b260ab60c09170dceaedd154e573bf30b
listopadgeorgii <listopadiogl@gmail.com>
2023-06-05 23:57:30 +0300

    refreshing previous lessons

--- src/stories/assets/Input.stories.tsx
+++ src/stories/assets/Input.stories.tsx
@@ -44,7 +44,9 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 export const ControlledInput = () => {
   const [parentValue, setParentValue] = useState('')
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>)=> { (setParentValue(e.currentTarget.value) ) }
+  const onChange = (e: ChangeEvent<HTMLInputElement>)=> {
+    (setParentValue(e.currentTarget.value) )
+  }
 
   return <input value={parentValue} onChange={onChange }/>
 
