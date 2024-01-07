71386a85b1521a64482b4d0df5f056485d2e1464
listopadgeorgii <listopadiogl@gmail.com>
2024-01-07 16:17:21 +0300

    25 - useEffect, cleanup

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -60,19 +60,20 @@ export const SimpleExample = () => {
 }
 
 
-export const SetTimeoutExample = () => {
+export const SetIntervalExample = () => {
 
     const [counter, setCounter] = useState(1)
 
     console.log('SetTimeoutExample')
 
-
-
-
     useEffect(()=> {
-        setInterval(()=> {
+     const interval =   setInterval(()=> {
             setCounter(state => state + 1)
         },1000)
+
+        return ()=> {
+         clearInterval(interval)
+        }
     }, [])
 
 
@@ -88,4 +89,70 @@ export const SetTimeoutExample = () => {
 
 }
 
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1)
+    console.log('Component rendered')
 
+    useEffect(()=> {
+        console.log('Effect occurred - ' + counter)
+
+
+        return ()=> {
+            console.log('RESET EFFECT ' + counter)
+        }
+
+    }, [counter])
+
+    return <>
+        Hello, counter : {counter} <button onClick={()=>{setCounter(counter + 1) } }>  +</button>
+    </>
+
+}
+
+export const KeysTrackerExample = () => {
+
+    const [text, setText] = useState('')
+
+
+    console.log('Component rendered with ' + text)
+
+    useEffect(()=> {
+        const handler = (e: KeyboardEvent)=> {
+            console.log(e.key)
+            setText(text + e.key)
+        }
+        window.addEventListener('keypress', handler)
+        return ()=> {
+            window.removeEventListener('keypress', handler)
+        }
+
+    }, [text])
+
+    return <>
+        Typed text : {text}
+    </>
+
+}
+
+export const SetTimeoutExample = () => {
+
+    const [text, setText] = useState('')
+
+
+    console.log('Component rendered with ' + text)
+
+    useEffect(()=> {
+        const timeoutId = setTimeout(()=> {
+            setText('3 seconds passed')
+        }, 3000)
+        return ()=> {
+            clearTimeout(timeoutId)
+        }
+
+    }, [text])
+
+    return <>
+        text : {text}
+    </>
+
+}
